64934ba5bf4eb01f883717f44727b4de8baf06ae
Tumaini <tumainikebaya.234@gmail.com>
2024-06-09 19:25:20 +0300

    Adds Header and Filter

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -10,16 +11,13 @@ function App() {
   }
 
   return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-      <ShoppingList items={itemData} />
-    </div>
-  );
+  <div className={"App " + (isDarkMode ? "dark" : "light")}>
+    <Header onDarkModeClick={onDarkModeClick} isDarkMode={isDarkMode} />
+    <ShoppingList items={itemData} />
+  </div>  
+  )
+    
+  ;
 }
 
 export default App;
